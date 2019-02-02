eb4629009e0f3351279e17c46afca84f0ad4c150
BitBlocksProject <35640215+BitBlocksProject@users.noreply.github.com>
2019-02-02 02:20:19 -0200

    Update splashscreen.cpp

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -41,7 +41,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
     QString copyrightTextDash = QChar(0xA9) + QString(" 2014-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Dash Core developers"));
     QString copyrightTextPivx = QChar(0xA9) + QString(" 2015-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The PIVX Core developers"));
     QString copyrightTextLpc = QChar(0xA9) + QString(tr(" 2018 LightPayCoin developers"));
-	QString copyrightTextBbk = QChar(0xA9) + QString(tr(" 2019 The BitBlocks Core developers"));
+    QString copyrightTextBbk = QChar(0xA9) + QString(tr(" 2018-2019 The BitBlocks Core developers"));
     QString titleAddText = networkStyle->getTitleAddText();
 
     QString font = QApplication::font().toString();
