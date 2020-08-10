4fc7605f45ecedeca416b0c4989be8ebb1c1437b
Szhenbbk <edsoneto.50@gmail.com>
2020-08-10 16:09:34 -0300

    Allow Swifttx with 10000 BBK

--- src/spork.h
+++ src/spork.h
@@ -43,7 +43,7 @@ using namespace boost;
 
 #define SPORK_2_SWIFTTX_DEFAULT 978307200                         //2001-1-1
 #define SPORK_3_SWIFTTX_BLOCK_FILTERING_DEFAULT 1424217600        //2015-2-18
-#define SPORK_5_MAX_VALUE_DEFAULT 1000                            //1000 BBK
+#define SPORK_5_MAX_VALUE_DEFAULT 10000                           //10000 BBK
 #define SPORK_7_MASTERNODE_SCANNING_DEFAULT 978307200             //2001-1-1
 #define SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT_DEFAULT 4070908800 //OFF
 #define SPORK_9_MASTERNODE_BUDGET_ENFORCEMENT_DEFAULT 4070908800  //OFF
@@ -54,7 +54,7 @@ using namespace boost;
 #define SPORK_14_NEW_PROTOCOL_ENFORCEMENT_DEFAULT 4070908800      //OFF
 #define SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2_DEFAULT 4070908800    //OFF
 #define SPORK_16_MN_WINNER_MINIMUM_AGE_DEFAULT 8000               // Age in seconds. This should be > MASTERNODE_REMOVAL_SECONDS to avoid
-                                                                  // misconfigured new nodes in the list. 
+                                                                  // misconfigured new nodes in the list.
                                                                   // Set this to zero to emulate classic behaviour
 
 class CSporkMessage;
